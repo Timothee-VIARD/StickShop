a4bfc9a5f4d2b7e62a357a141123edc9f84cc187
Timothée VIARD <timothee.viard@reseau.eseo.Fr>
2024-03-13 15:23:52 +0100

    Init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Banner/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,5 @@
+const Banner = () => (
+    <h1>AHHHHHHHHHH HHAHHAHHAHAHAHHAHAH HAHH AHA HA </h1>
+);
+
+export default Banner;
