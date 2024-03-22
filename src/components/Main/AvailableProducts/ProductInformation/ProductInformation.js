5852fb7d434c37267424f74a228f6391f891e6b4
Timothée VIARD <timothee.viard@reseau.eseo.Fr>
2024-03-22 16:51:02 +0100

    Refacto Modal

--- src/components/Main/AvailableProducts/ProductInformation/ProductInformation.js
+++ src/components/Main/AvailableProducts/ProductInformation/ProductInformation.js
@@ -1,7 +1,8 @@
-import React, {useContext} from "react";
-import {Button, Card, CardActions, CardContent, CardMedia, Modal, Typography} from "@mui/material";
+import React, {useContext, useRef, useState} from "react";
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Modal, Typography} from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import {CartContext} from "../../../../context/CartContext";
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 
 /**
  * Commponent to have more information for a product
@@ -10,44 +11,59 @@ const ProductInformation = ({product, isOpen, handleOpenModal}) => {
 
   const {addToCart} = useContext(CartContext);
 
+  const [isBottom, setIsBottom] = useState(false);
+  const modalRef = useRef(null);
+
+  const handleScroll = (e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop - 40 <= e.target.clientHeight;
+    setIsBottom(bottom);
+  }
+
   return (
     <Modal open={isOpen} onClose={handleOpenModal} className="flex flex-row justify-center items-center">
-      <Card className="rounded-2xl w-1/2 h-5/6 overflow-auto">
-        <CardMedia
-          sx={{height: 280}}
-          image={product.image}
-          title={product.name}
-          onClick={handleOpenModal}
-        />
-        <CardContent className="bg-amber-100 bg-opacity-50 p-10 ">
-          <Typography gutterBottom variant="h3" component="div">
-            {product.name}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {`Prix : ${product.price} €`}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {`Description : ${product.description}`}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {`Stock : ${product.quantity}`}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {`Catégorie : ${product.category}`}
-          </Typography>
-          <Typography gutterBottom variant="h6" component="div">
-            {`Note : ${product.rating}`}
-          </Typography>
-        </CardContent>
-        <CardActions className="bg-amber-100 bg-opacity-50 p-10">
-          <Button size="small" color="inherit" className="text-black gap-3 rounded-2xl" onClick={() => addToCart(product)}>
-            <ShoppingCartIcon/>
-            <Typography variant="p">
-              Ajouter au panier
+      <Box className="w-1/2 h-5/6 relative">
+        <Card className="h-full overflow-auto rounded-2xl scrollbar-hide" onScroll={handleScroll} ref={modalRef}>
+          <CardMedia
+            sx={{height: 380}}
+            image={product.image}
+            title={product.name}
+            onClick={handleOpenModal}
+          />
+          <CardContent className="bg-amber-100 bg-opacity-50 p-10">
+            <Typography gutterBottom variant="h3" component="div">
+              {product.name}
             </Typography>
-          </Button>
-        </CardActions>
-      </Card>
+            <Typography gutterBottom variant="h6" component="div">
+              {`Prix : ${product.price} €`}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {`Description : ${product.description}`}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {`Stock : ${product.quantity}`}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {`Catégorie : ${product.category}`}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {`Note : ${product.rating}`}
+            </Typography>
+          </CardContent>
+          <CardActions className="bg-amber-100 bg-opacity-50 p-10">
+            <Button size="small" color="inherit" className="text-black gap-3 rounded-2xl p-5" onClick={() => addToCart(product)}>
+              <ShoppingCartIcon/>
+              <Typography variant="p">
+                Ajouter au panier
+              </Typography>
+            </Button>
+          </CardActions>
+        </Card>
+        {!isBottom && (
+          <Box className="flex flex-col items-center bg-black bg-opacity-5">
+            <KeyboardArrowDownRoundedIcon className="absolute bottom-0 w-full rounded-b-2xl h-12" fontSize="large"/>
+          </Box>
+        )}
+      </Box>
     </Modal>
   );
 };
