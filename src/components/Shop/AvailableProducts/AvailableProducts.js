8989559410684914518a4211661af9749188d83d
Timothée Viard <timothee.viardtv@gmail.com>
2024-04-07 23:27:20 +0200

    Add transitions for the products' shop

--- src/components/Shop/AvailableProducts/AvailableProducts.js
+++ src/components/Shop/AvailableProducts/AvailableProducts.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Grid} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Grid, Grow } from '@mui/material';
 import Product from './Product/Product';
 
 /**
@@ -7,21 +7,27 @@ import Product from './Product/Product';
  */
 const AvailableProducts = () => {
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('/articles/products.json');
       const data = await response.json();
       setProducts(data.products);
+      setChecked(true);
     };
     fetchProducts();
   }, []);
 
   return (
     <Grid container spacing={2} className="px-4 pb-4">
-      {products.map((product) => (
+      {products.map((product, index) => (
         <Grid item xs={12} sm={6} lg={3} key={product.id}>
-          <Product product={product}/>
+          <Grow in={checked} timeout={index * 100}>
+            <Box>
+              <Product product={product} />
+            </Box>
+          </Grow>
         </Grid>
       ))}
     </Grid>
