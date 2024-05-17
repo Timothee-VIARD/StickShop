d506622686af4bd51c362dac5b98f18d9ecbed70
Timothée Viard <timothee.viardtv@gmail.com>
2024-05-17 14:39:21 +0200

    BackEnd - Add swagger doc on the routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,13 @@ import http from 'http';
 import express, * as bodyParser from 'express';
 import router from './routes/router.js';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import specs from './swaggerConfig.js';
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 app.use(cors());
 app.use(bodyParser.json());
 
